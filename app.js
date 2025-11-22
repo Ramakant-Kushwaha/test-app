6b185fba0097437a9f5a07f3e976a53662f42562
Ramakant Kushwaha <ramakantkushwaha1995@gmail.com>
2025-11-22 22:50:57 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello From Web').status(200);
+});
+
+app.listen(3000, (err) => {
+	console.log('Server is 3000');
+});
