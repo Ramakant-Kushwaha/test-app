1bb03f53a3f816e952c63774dca536e8470d1118
Ramakant Kushwaha <ramakantkushwaha1995@gmail.com>
2025-11-22 22:52:24 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Hello From Web').status(200);
+	res.send({ msg: 'hello' }).status(200);
 });
 
 app.listen(3000, (err) => {
