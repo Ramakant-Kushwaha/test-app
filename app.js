f9c35e5fbd8b77a062e72024d287ca1ccdd266d7
Ramakant-Kushwaha <98745462+Ramakant-Kushwaha@users.noreply.github.com>
2025-11-23 23:54:30 +0530

    Change log message from 'hello' to 'hello 1'

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	console.log("hello");
+	console.log("hello 1");
 	res.send({ msg: 'hello' }).status(200);
 });
 
